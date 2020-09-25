18a4c1a1bb13de337378ec0b4dcbf621a6088871
RomanHumennyi <duklagos@gmail.com>
2020-09-25 16:25:02 +0000

    Sample code

---
+++ js/index.js
@@ -0,0 +1,22 @@
+document.querySelector('.timestamp').innerText = new Date().toLocaleTimeString();
+
+document.querySelector('.get-html-ajax').addEventListener('click', getHtmlAjax);
+
+function getHtmlAjax () {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200 ) {
+            document.querySelector('.html-container').innerHTML = xhr.responseText;
+        }
+    }
+    xhr.open('GET', 'client-data.html', true);
+    xhr.send();
+}
+
+document.querySelector('.fetch-html').addEventListener('click', fetchHtml);
+
+function fetchHtml() {
+    fetch('client-data.html')
+        .then( response => response.text() )
+        .then( html => document.querySelector('.html-container').innerHTML = html );
+}
